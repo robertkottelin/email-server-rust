1187968aa2c72ee9ed6fbca6857e683ee44cc9a1
robertkottelin <robert.kottelin@gmail.com>
2023-04-24 20:47:31 +0200

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use emailserverrust::run;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use std::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
